c504499f2d02b3e5ddbd43f5912e2cec0c5bde49
Kazefreedom <kazefreedom@gmx.at>
2016-03-14 22:05:54 +0100

    Update DB

--- laravel/_sql/g5bot_db.sql
+++ laravel/_sql/g5bot_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Mar 14, 2016 at 03:05 PM
--- Server version: 5.5.31
--- PHP Version: 5.4.16
+-- Host: localhost:3306
+-- Generation Time: Mar 14, 2016 at 02:02 PM
+-- Server version: 10.1.9-MariaDB-log
+-- PHP Version: 5.6.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,13 +14,48 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `g5bot_db`
 --
-CREATE DATABASE IF NOT EXISTS `g5bot_db` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `g5bot_db`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stats`
+--
+
+CREATE TABLE `stats` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `duration` time NOT NULL,
+  `description` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `stats`
+--
+
+INSERT INTO `stats` (`id`, `firstName`, `lastName`, `time`, `duration`, `description`) VALUES
+(1, 'Patrick', 'Keller', '2016-03-14 19:57:04', '00:30:00', 'Test'),
+(2, 'Alen', 'Kocaj', '2016-03-14 19:59:48', '00:00:00', 'Test_Test'),
+(3, 'Michael', 'Ester', '2016-03-14 19:59:48', '01:30:00', 'Validation'),
+(4, 'Daniel', 'Stigler', '2016-03-14 19:59:48', '00:00:50', 'HAHA'),
+(5, 'Patrick', 'Keller', '2016-03-14 20:15:52', '01:50:00', 'TESTnachTest');
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `statsview`
+--
+CREATE TABLE `statsview` (
+`id` int(11)
+,`lastName` varchar(50)
+,`timeSpent` time
+);
 
 -- --------------------------------------------------------
 
@@ -28,18 +63,56 @@ USE `g5bot_db`;
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `firstName` varchar(50) NOT NULL,
   `lastName` varchar(50) NOT NULL,
   `email` varchar(150) NOT NULL,
   `password` varchar(300) NOT NULL,
   `active` int(11) NOT NULL DEFAULT '0',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Structure for view `statsview`
+--
+DROP TABLE IF EXISTS `statsview`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `statsview`  AS  select `stats`.`id` AS `id`,`stats`.`lastName` AS `lastName`,sec_to_time(sum(time_to_sec(`stats`.`duration`))) AS `timeSpent` from `stats` group by `stats`.`lastName` order by `stats`.`lastName` ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `stats`
+--
+ALTER TABLE `stats`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `stats`
+--
+ALTER TABLE `stats`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
